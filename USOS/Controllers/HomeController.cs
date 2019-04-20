04e8b307ebeed774fc37ed4456c943c8ca317fc2
grzegorzgrabiec <grzegorzgrabiec@wp.pl>
2019-04-20 13:58:53 +0200

    test

--- USOS/Controllers/HomeController.cs
+++ USOS/Controllers/HomeController.cs
@@ -4,24 +4,40 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
 using USOS.Models;
+using Microsoft.Extensions.Configuration;
 
 namespace USOS.Controllers
 {
+
     public class HomeController : Controller
     {
+        
+        private readonly IConfiguration configuration;
+        SqlConnection con;
+        SqlCommand com;
+        SqlDataReader dr;
+
+        public HomeController(IConfiguration config)
+        {
+            this.configuration = config;
+        }
+        
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult About()
+        public IActionResult Login()
         {
             ViewData["Message"] = "Your application description page.";
 
             return View();
         }
 
+        [HttpGet]
+       
         public IActionResult Contact()
         {
             ViewData["Message"] = "Your contact page.";
@@ -33,31 +49,36 @@ namespace USOS.Controllers
         {
             return View();
         }
+        
+       
+        [HttpPost]
+        public ActionResult Verify(Account acc)
+        {
+        string connStr = configuration.GetConnectionString("MyConnStr");
+        con = new SqlConnection(connStr);
+        com = new SqlCommand();
+            con.Open();
+            com.Connection = con;
+            com.CommandText = "select * from Users where Login='" + acc.Login + "' and Password='" + acc.Password + "'";
+            dr = com.ExecuteReader();
+            if(dr.Read())
+            {
+                con.Close();
+               
+                return View("Akitek");
+            }
+            else
+            {
+                con.Close();
+                return View("Login");
+            }
+           
 
-        public IActionResult Test()
-        {
-            return View();
+
+            
+
         }
-        public IActionResult KG()
-        {
-            return View();
-        }
-        public IActionResult KG2()
-        {
-            return View();
-        }
-        public IActionResult GG2()
-        {
-            return View();
-        }
-        public IActionResult Test22()
-        {
-            return View();
-        }
-        public IActionResult Test2()
-        {
-            return View();
-        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
